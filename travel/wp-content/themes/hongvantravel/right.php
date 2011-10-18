aa1de7efc0e5f0921a5843f8cba10d0a8b639960
vphat28 <vphat28@gmail.com>
2011-10-18 13:29:31 +0000

    them flash

--- travel/wp-content/themes/hongvantravel/right.php
+++ travel/wp-content/themes/hongvantravel/right.php
@@ -88,5 +88,52 @@
                                         </p>
 
                                     </div>
-                                    <p align="center"><img src="<?php bloginfo('template_url') ?>/images/gif.gif" /></p>
+                                    <p align="center">  <object
+
+        classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"
+
+        codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,19,0"
+
+        id="Movie2"
+
+        width="155" height="250"
+
+      >
+
+        <param name="movie" value="Movie2.swf">
+
+        <param name="bgcolor" value="#FFFFFF">
+
+        <param name="quality" value="high">
+
+        <param name="allowscriptaccess" value="samedomain">
+
+        <embed
+
+          type="application/x-shockwave-flash"
+
+          pluginspage="http://www.adobe.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash"
+
+          name="Movie2"
+
+          width="155" height="250"
+
+          src="Movie2.swf"
+
+          bgcolor="#FFFFFF"
+
+          quality="high"
+
+          allowscriptaccess="samedomain"
+
+        >
+
+          <noembed>
+
+          </noembed>
+
+        </embed>
+
+      </object>
+</p>
                                 </div>
