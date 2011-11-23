3885596f29d5ac6846d1ca56a1f94c092a5ce887
cuonghh.2121989 <cuonghh.2121989@gmail.com>
2011-11-23 11:25:31 +0000

    update

--- diaoc/protected/views/layouts/main.php
+++ diaoc/protected/views/layouts/main.php
@@ -82,5 +82,73 @@
         </div>
 
     </body>
+<script type="text/javascript">
 
+$().ready(function(){
+        $('.btdelete_thanhpho').click(function() {
+            //var t = confirm('Bạn thấy có muốn xóa tỉnh thành này không');
+            // if(t){
+            var id=$(this).attr('id');
+            $.ajax({
+                type:'POST',
+                data:'id='+id,
+                url:'index.php?r=quantri/QuanLyTinhThanhQuanHuyenPhuongXa/Xoatinhthanh',
+                success: function (html){
+                    $('#Grid_tinh_thanh_pho').html(html);
+                    return false;
+                }
+
+            })
+        });
+        $('.btdelete_qh').click(function() {
+                var idtp= $('#hiddenTTP').val();
+               // var t = confirm('Bạn thấy có muốn xóa quận huyện này không');
+                //if(t){
+                    var id=$(this).attr('id');
+                    $.ajax({
+                        type:'POST',
+                        data:'idqh='+id+'&idtp='+idtp,
+                        url:'index.php?r=quantri/QuanLyTinhThanhQuanHuyenPhuongXa/XoaQuanHuyen',
+                        success: function (html){
+                            $('#Grid_qh').html(html);
+                            return false;
+                        }
+
+                    })
+               // }
+            });
+
+         $('.row_ttp').click(function(){
+           
+            var id=$(this).attr('id');
+            $('#hiddenTTP').val(id);
+             $.ajax({
+                    type:'POST',
+                    data:'idtp='+id,
+                    url:'index.php?r=quantri/QuanLyTinhThanhQuanHuyenPhuongXa/LoadQuanHuyen',
+                    success: function (html){
+                        $('#Grid_qh').html(html);
+                        return false;
+                    }
+
+                })
+        });
+         $('.row_qh').click(function(){
+
+            var id=$(this).attr('id');
+            $('#hiddenQH').val(id);
+             $.ajax({
+                    type:'POST',
+                    data:'idqh='+id,
+                    url:'index.php?r=quantri/QuanLyTinhThanhQuanHuyenPhuongXa/Loadphuongxa',
+                    success: function (html){
+                        $('#Grid_px').html(html);
+                        return false;
+                    }
+
+                })
+        });
+});
+         
+    </script>
 </html>
