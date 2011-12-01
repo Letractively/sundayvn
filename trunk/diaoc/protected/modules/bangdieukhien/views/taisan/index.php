ec3a848627b2e1d062aee143259a6ee24db900e8
vphat28@gmail.com <vphat28@gmail.com@14f92771-19bc-0641-bd4e-6e02e7467f47>
2011-12-01 16:34:12 +0000

    cập nhật layout cho bảng đăng tài sản, khuyến cáo anh em khi css thì các chỉ số tốt nhất nên xài % thay vì px vì giao diện sẽ linh hoạt dễ chỉnh sửa hơn

--- trunk/diaoc/protected/modules/bangdieukhien/views/taisan/index.php
+++ trunk/diaoc/protected/modules/bangdieukhien/views/taisan/index.php
@@ -1,4 +1,3 @@
-
 <div id="bang_dang_taisan" class="bangdieukhien_module">
     <div class="box_header">
         <h3>Đăng tài sản</h3>
@@ -7,5 +6,45 @@
         <h4>
             Thông tin cơ bản
         </h4>
+        <p><b>Điền chính xác các thông tin dưới đây giúp cho tài sản của bạn xuất hiện chính xác và đầy đủ trong các kết quả theo nhu cầu của người dùng, việc này giúp cho giao dịch của bạn sẽ nhanh hơn.</b></p>
+        <div class="form_row clearfix">
+            <div class="input_title">Loại tin rao:</div>
+            <div class="input_content chonloaitin">
+                <input type="radio" id="radio_loai_tin_rao_can_ban" value="B" name="loai_tin_rao" />
+                <label for="radio_loai_tin_rao_can_ban">Cần bán</label>
+                <input type="radio" id="radio_loai_tin_rao_can_mua" value="M" name="loai_tin_rao" />
+                <label for="radio_loai_tin_rao_can_mua">Cần mua</label>
+                <input type="radio" id="radio_loai_tin_rao_can_thue" value="T" name="loai_tin_rao" />
+                <label for="radio_loai_tin_rao_can_thue">Cần thuê</label>
+                <input type="radio" id="radio_loai_tin_rao_cho_thue" value="CT" name="loai_tin_rao" />
+                <label for="radio_loai_tin_rao_cho_thue">Cho thuê</label>
+            </div>
+        </div>
+        <div class="form_row clearfix">
+            <div class="input_title">Loại địa ốc:<span class="required">*</span></div>
+            <div class="input_content chonloaidiaoc">
+                <div class="twocol clearfix">
+                    <div class="tcleft">
+                        <div class="customize">
+                            <select name="loaidiaoc">
+                                <option>Biệt thự</option>
+                                <option>Nhà phố</option>
+                                <option></option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="tcright">
+                        <div class="customize">
+                            <select name="vitridiaoc">
+                                <option>Mặt tiền</option>
+                                <option>Mặt hẻm</option>
+                                <option>Hẻm lớn</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="extra">&nbsp;</div>
+            </div>
+        </div>
     </div>
 </div>
