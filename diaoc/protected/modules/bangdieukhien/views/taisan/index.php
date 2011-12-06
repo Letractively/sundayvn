24c72f061c078376e4971a2cbf15a6e61262a7a5
vphat28 <vphat28@gmail.com>
2011-12-06 16:35:20 +0000

    tiep tuc trang dang tin

--- diaoc/protected/modules/bangdieukhien/views/taisan/index.php
+++ diaoc/protected/modules/bangdieukhien/views/taisan/index.php
@@ -3,9 +3,8 @@
         <h3>Đăng tài sản</h3>
     </div>
     <div class="box-content">
-        <h4>
-            Thông tin cơ bản
-        </h4>
+        <h4>Thông tin cơ bản</h4>
+
         <p><b>Điền chính xác các thông tin dưới đây giúp cho tài sản của bạn xuất hiện chính xác và đầy đủ trong các kết quả theo nhu cầu của người dùng, việc này giúp cho giao dịch của bạn sẽ nhanh hơn.</b></p>
         <div class="form_row clearfix">
             <div class="input_title">Loại tin rao:</div>
@@ -93,7 +92,7 @@
                 <div class="twocol clearfix">
                     <div class="tcleft">
                         <div class="customize border_input">
-                            <input type="text" value="Vui lòng nhập số nhà" />
+                            <input type="text" style="border: none;"  value="Vui lòng nhập số nhà" />
                         </div>
                     </div>
                     <div class="tcright">
@@ -113,7 +112,7 @@
         <!--Ten du an-->
         <div class="form_row clearfix">
             <div class="input_title">Tên dự án:</div>
-            <div class="input_content chonloaidiaoc">
+            <div class="input_content">
                 <select name="vitridiaoc">
                     <option>Green Sun Office</option>
                     <option>Hà Nội</option>
@@ -121,7 +120,65 @@
                 </select>
 
             </div>
-             <div class="extra">Khác&nbsp;<input type="checkbox" /></div>
+            <div class="extra">Khác&nbsp;<input type="checkbox" /></div>
+        </div>
+        <!--Gia-->
+        <div class="form_row clearfix">
+            <div class="input_title">Giá:</div>
+            <div class="input_content xacdinhgia">
+                <div class="gia ctr">
+                    <input type="text"  />
+                </div>
+                <div class="donvigia ctr">
+                    <select name="donvigia">
+                        <option>VND</option>
+                        <option>USD</option>
+                        <option>SJC</option>
+                    </select>
+                </div>
+                <div class="tongdientich ctr">
+                    <select name="tongdientich">
+                        <option>Tổng diện tích</option>
+                        <option>m2</option>
+                        <option>Tháng</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+        <!--mAT TIEN-->
+        <div class="form_row clearfix">
+            <div class="input_title">Môi giới:</div>
+            <div class="input_content chonloaitin">
+                <input type="radio" id="mien_trung_gian_radio" value="MTG" name="loai_moi_gioi">
+                <label for="mien_trung_gian_radio"><b>Miễn trung gian</b></label>
+                <input type="radio" id="ky_goi_moi_gion" value="KGMG" name="loai_moi_gioi">
+                <label for="ky_goi_moi_gion"><b>Ký gởi môi giới</b></label>
+            </div>
+        </div>
+        <!--Diện tích sử dụng-->
+        <div class="form_row clearfix">
+            <div class="input_title">Diện tích sử dụng: <span class="required">*</span></div>
+            <div class="input_content">
+                <input type='text' style="width: 20%;" value="0" /><span>&nbsp;m2</span>
+            </div>
+        </div>
+        <!--Diện tích khuôn viên-->
+        <div class="form_row clearfix">
+            <div class="input_title">Diện tích khuôn viên: <span class="required">*</span></div>
+            <div class="input_content">
+                <input type='text' style="width: 20%;" value="Chiều ngang" /><span>&nbsp;m&nbsp;<strong>X</strong>&nbsp;</span><input type='text' style="width: 20%;" value="Chiều rộng" /><span>&nbsp;m</span><input type="checkbox" id="dien_tich_khuon_vien_nohau" /><label for="dien_tich_khuon_vien_nohau"><b>Nở hậu</b></label>
+            </div>
+        </div>
+        <!--Diện tích xây dựng-->
+        <div class="form_row clearfix">
+            <div class="input_title">Diện tích xây dựng: <span class="required">*</span></div>
+            <div class="input_content">
+                <input type='text' style="width: 20%;" value="Chiều ngang" /><span>&nbsp;m&nbsp;<strong>X</strong>&nbsp;</span><input type='text' style="width: 20%;" value="Chiều rộng" /><span>&nbsp;m</span><input type="checkbox" id="dien_tich_xay_dung_nohau" /><label for="dien_tich_xay_dung_nohau"><b>Nở hậu</b></label>
+            </div>
+        </div>
+        <h4>Đặc điểm &amp; tiện ích</h4>
+        <div id="dacdiem_dientich_panel" class="clearfix">
+            
         </div>
     </div>
 </div>
