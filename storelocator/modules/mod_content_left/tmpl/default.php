8ba2f649e390a67c5af38661b1380bf9ce6bd5c4
vphat28 <vphat28@gmail.com>
2012-12-16 14:58:24 +0000

    more todays

--- storelocator/modules/mod_content_left/tmpl/default.php
+++ storelocator/modules/mod_content_left/tmpl/default.php
@@ -3,47 +3,32 @@
 defined('_JEXEC') or die;
 $filter_zone = JRequest::getVar("filter_zone");
 $filter_type = JRequest::getVar("filter_type");
+
+//$geoData =  (unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$_SERVER['REMOTE_ADDR'])));
+$geoData =  JRequest::getVar('geoData');
+
 ?>
 
 <div class="blockSearch">
     <h3><?php echo JText::_('MOD_CONTENT_LEFT_LOCATE_US'); ?></h3>
     <div class="listItemSearch">
         <strong class="titSearchBy"><?php echo JText::_('MOD_CONTENT_LEFT_SEARCH_BY'); ?></strong>
-        <form action="<?php echo JRoute::_('index.php?option=com_googlemaplocator&view=location&layout=show'); ?>" method="post" id="frmSearch" name="frmSearch" class="frmSearch">
+        <form action="<?php echo JRoute::_('index.php?option=com_googlemaplocator&view=location&layout=show'); ?>" method="post" id="frmSearch" name="frmSearch" class="frmSearch" onsubmit="return false">
             <ul>
                 <li class="lineSpec">
                     <label for="txtAddress"><?php echo JText::_('MOD_CONTENT_LEFT_ADDRESS'); ?></label>
-                    <input type="text" id="filter_address" name="filter_address" value="" />
+                    <input type="text" id="filter_address" name="filter_address" value="<?php echo $geoData['geoplugin_city']; ?>" />
                 </li>
-                <li>
-                    <label for="txtZone"><?php echo JText::_('MOD_CONTENT_LEFT_ZONE'); ?></label>
-                    <select name="filter_zone" id="filter_zone" class="inputbox">
-                        <option value=""><?php echo JText::_('MOD_CONTENT_LEFT_SELECT_ZONE'); ?></option>
-                        <?php echo JHtml::_('select.options', $list_filter_zone); ?>
-                    </select>
-                </li>
-                <li>
-                    <label for="txtType"><?php echo JText::_('MOD_CONTENT_LEFT_TYPE'); ?></label>
-                    <select name="filter_type" id="filter_type" class="inputbox">
-                        <option value=""><?php echo JText::_('MOD_CONTENT_LEFT_SELECT_TYPE'); ?></option>
-                        <?php echo JHtml::_('select.options', $list_filter_type); ?>
-                    </select>
-                </li>
-                <li>
-                    <label for="txtServices"><?php echo JText::_('MOD_CONTENT_LEFT_SERVICES'); ?></label>
-                    <select name="filter_service" id="filter_service" class="inputbox">
-                        <option value=""><?php echo JText::_('MOD_CONTENT_LEFT_SELECT_SERVICES'); ?></option>
-                        <?php echo JHtml::_('select.options', $list_filter_service); ?>
-                    </select>
-                </li>
-                <li class="floatR">
+				<li class="lineSpec">
+<h4 ><?php echo $geoData['geoplugin_city']; ?></h4>
+                    
+			</li>
+               <li class="floatR">
                     <span class="btn btn-1">
                         <span>
-                            <?php if (JRequest::getString('view') != "location" || JRequest::getString('option') != "com_googlemaplocator") { ?>
-                                <input type="submit" id="btnSearch" name="submit_value" value="Search" title="<?php echo JText::_('MOD_CONTENT_LEFT_SEARCH'); ?>" />
-                            <?php } else { ?>
+                          
                                 <input type="button" id="btnSearch" name="submit_value" value="Search" title="<?php echo JText::_('MOD_CONTENT_LEFT_SEARCH'); ?>" />
-                            <?php } ?>
+        
                         </span>
                     </span><!--btn-->
                 </li>
